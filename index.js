02b8fb375eff3632685e54ae3a4c9d11d7a5cb3d
Philippe Moneda <philippe@MacBook-Pro-de-Philippe.local>
2016-01-03 11:08:56 -0200

    adicionando pagina do editor

--- index.js
+++ index.js
@@ -10,13 +10,32 @@ var http = require('http');
 var userArgs = process.argv.slice(2);
 var finalhandler = require('finalhandler');
 var serveStatic = require('serve-static');
-
+var request = require('request');
 var serve = serveStatic("./");
 
 var server = http.createServer(function(req, res) {
   var done = finalhandler(req, res);
-  serve(req, res, done);
+  if(isWebEditor(req)){
+    handleWebEditor(req,res);  
+  }else{
+    serve(req, res, done);    
+  }    
 });
+
+
+function isWebEditor(request){
+    return request.url.indexOf("editor") > 0;
+};
+
+function handleWebEditor(req,res){
+    request('http://www.google.com', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(body);
+        
+      }
+    })
+};
 
 server.listen(8000);
 
