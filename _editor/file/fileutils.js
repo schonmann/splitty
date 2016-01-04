ae50e1017ca10e3171dae1648568a826a9a21bb8
Philippe Moneda <philippe@MacBook-Pro-de-Philippe.local>
2016-01-03 22:11:26 -0200

    ajustes no editor e possibilidade de execucao de comandos shell a partir do browser

---
+++ _editor/file/fileutils.js
@@ -0,0 +1,16 @@
+var FileUtils = (function(){
+    var self = {};
+    self.open = function(fileName){};
+    self.ls = function(){
+        exec("ls");
+    };
+    self.exec = function(command){
+        exec(command);
+    };
+    function exec(cmd){
+        socket.emit("command", {command:cmd});
+    };
+    
+    
+    return self;
+})();
--- index.js
+++ index.js
@@ -6,48 +6,46 @@
     A cereja do bolo seria criar um modulo que permitisse ate a edicao de arquivo dentro do browser e tambem com o live preview dentro
     da mesma pagina, seria bem doido
 */
-var http = require('http');
-var userArgs = process.argv.slice(2);
+//var userArgs = process.argv.slice(2);
+
+var http         = require('http');
 var finalhandler = require('finalhandler');
-var serveStatic = require('serve-static');
-var request = require('request');
-var serve = serveStatic("./");
-var fs = require("fs");
+var serveStatic  = require('serve-static');
+var request      = require('request');
+var serve        = serveStatic("./");
+var editor       = serveStatic(__dirname);
+var fs           = require("fs");
+var sys          = require('sys')
+var exec         = require('child_process').exec;
+var child;
+
 var server = http.createServer(function(req, res) {
   var done = finalhandler(req, res);
-  if(isWebEditor(req)){
-    handleWebEditor(req,res);  
+  if(isWebEditor(req)){      
+    editor(req,res,done);
   }else{
     serve(req, res, done);    
   }    
 });
 var io = require('socket.io')(server);
 
-function isWebEditor(request){
+function isWebEditor(request){        
     return request.url.indexOf("_editor") > 0;
 };
-
-function handleWebEditor(req,res){
-    request('https://raw.githubusercontent.com/shortty/nodeit/master/editor/index.html', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(body);
-        
-      }
-    })
-};
-
 io.on('connection', function (socket) {
     socket.on('fileSave', function (data) {
-        fs.writeFile(__dirname + data.filePath, data.lines.join('\r\n'));
-        
+        fs.writeFile(process.cwd() + data.filePath, data.lines.join('\r\n'));
+    });
+    
+    socket.on('command',function(data){
+        console.log(data.command);
+        child = exec(data.command, function (error, stdout, stderr) {
+          socket.emit("stdout",stdout);
+        });
     });
     
 });
-
-
 server.listen(8000);
-
 
 
 
