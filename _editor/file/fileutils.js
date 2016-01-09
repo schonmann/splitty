a0094e15532f2c8daee650187322bd8251c177f6
Philippe Moneda <philippe@MacBook-Pro-de-Philippe.local>
2016-01-09 09:19:07 -0200

    modificando para o ES6

--- _editor/file/fileutils.js
+++ _editor/file/fileutils.js
@@ -1,14 +1,33 @@
 var FileUtils = (function(){
+    
     var self = {};
-    self.open = function(fileName){};
-    self.ls = function(){
-        exec("ls");
+    var _editor = null;
+    var _socket = null;
+    var openedFiles = ['/readme.txt'];
+    var currentFile = 0;
+    
+    self.setEditor = (editor) => _editor = editor;
+    
+    self.setSocket = (socket) => _socket = socket;
+    
+    self.bind = () => {
+        _editor.env.document.on("change",function(e){        
+            _socket.emit('fileSave', {filePath: openedFiles[currentFile], lines: editor.getSession().doc.$lines});        
+        });
+        _editor.commands.addCommand({
+            name: "replace",
+            bindKey: {win: "Ctrl-O", mac: "Command-O"},
+            exec: Shell.ls
+        });
     };
-    self.exec = function(command){
-        exec(command);
+    
+    self.open = (fileName) => {};
+    
+    self.setup = (editor,socket) =>{
+        self.setEditor(editor);
+        self.setSocket(socket);
+        self.bind();
     };
-    function exec(cmd){
-        socket.emit("command", {command:cmd});
-    };
+    
     return self;
 })();
