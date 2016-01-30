7afdd790a065c3a81ae5332f9351bc841a730d3f
Philippe Moneda <philippemoneda@gmail.com>
2016-01-30 12:48:03 -0200

    ajustes para armazenar o estado do editor em relacao ao tamanho

--- _editor/actions/proportionaction.js
+++ _editor/actions/proportionaction.js
@@ -2,6 +2,7 @@ var ProportionAction = (()=>{
     var self = {};
     
     self.execute = (value) => {
+        NodeIT.prop("proportion",value)
         var frame = window.parent.document.getElementById("rightSideFrame")
         var editor = window.parent.document.getElementById("frameEditor")
         frame.setAttribute('width',value + '%')
@@ -11,7 +12,14 @@ var ProportionAction = (()=>{
     self.init = (txtValue) => {txtValue.value = ""}
     self.getLabelAction = () => "right side size"
     
+    
+    self.startup = () => {
+        if(NodeIT.hasProp("proportion")){
+            self.execute(NodeIT.prop("proportion"))
+        }
+    }
      
     Shortcut.bindEvent("proportion",{win: "Ctrl+Shift-P", mac: "Command+Shift-P"},self)
+    NodeIT.register(self)
     return self;
 })();
