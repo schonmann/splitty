c5c115166870bf6bbbcdcbca4d240ec28e31db3c
Philippe Moneda <philippemoneda@gmail.com>
2016-02-08 13:24:38 -0200

    adicionando ; nas linhas

--- _editor/actions/terminalaction.js
+++ _editor/actions/terminalaction.js
@@ -1,89 +1,89 @@
 var TerminalAction = (()=>{
     var self = {}
     self.execute = (value) => {
-        if (value.trim() === "") return 1
+        if (value.trim() === "") return 1;
         if(value.startsWith("alias:")){
-            self.defineAlias(value)
-            document.getElementById('optionValue').value = ""
+            self.defineAlias(value);
+            document.getElementById('optionValue').value = "";
         }else if(self.hasAliasFromCommand(value)){
-            self.exec(self.compile(value))
+            self.exec(self.compile(value));
            
         }else{
-            self.exec(value)
+            self.exec(value);
         }
-        return 1
-    }  
+        return 1;
+    };  
     
     self.escape = (html) => {
         return document.createElement('div')
             .appendChild(document.createTextNode(html))
             .parentNode
-            .innerHTML
-    }
+            .innerHTML;
+    };
 
     
     self.exec = (value) => {
-        ctxout.innerHTML = ""
+        ctxout.innerHTML = "";
         Shell.exec(value, (stdout) => {
-            var ctxout = document.getElementById('ctxout')
-            ctxout.focus()
-            ctxout.style.display = "block"
-            ctxout.innerHTML = "<pre>"+self.escape(Shell.stdio())+"</pre>"
-        })
-        document.getElementById('optionValue').value = ""
-    }
+            var ctxout = document.getElementById('ctxout');
+            ctxout.focus();
+            ctxout.style.display = "block";
+            ctxout.innerHTML = "<pre>"+self.escape(Shell.stdio())+"</pre>";
+        });
+        document.getElementById('optionValue').value = "";
+    };
     self.compile = (command) => {
         /*This method compiles command and aliases indentifying alias params*/
         
         var commands = command.split("&&");
-        var toExec = []
+        var toExec = [];
         commands.each((cmd) => {
-            var tokens = self.tokenizeCommand(command)
-            var alias = self.getAlias(tokens[0])
+            var tokens = self.tokenizeCommand(command);
+            var alias = self.getAlias(tokens[0]);
             for(var i = 1; i < tokens.length; i++){
-                alias = alias.replace("$"+i,tokens[i])
+                alias = alias.replace("$"+i,tokens[i]);
             }
-            toExec.push(alias)
-        })
-        var shellCommand = toExec.join(";")
-        return shellCommand
-    }
+            toExec.push(alias);
+        });
+        var shellCommand = toExec.join("&&");
+        return shellCommand;
+    };
     
     self.hasAliasFromCommand = (command) => {
-        var tokens = self.tokenizeCommand(command)
-        return self.hasAlias(tokens[0])
-    }
+        var tokens = self.tokenizeCommand(command);
+        return self.hasAlias(tokens[0]);
+    };
     
     self.tokenizeCommand = (command) => {
         var tokens = [];
-        var currentToken = ""
+        var currentToken = "";
         var index = 0;
         for(var c = 0; c < command.length; c++){
             if(command[c]=="\""){
-                index = c + 1
-                currentToken = "\""
+                index = c + 1;
+                currentToken = "\"";
                 while(command[index] != "\"" && index < command.length){
-                    currentToken += command[index]
-                    index++
+                    currentToken += command[index];
+                    index++;
                 }
                 if(currentToken === "\"")continue;
                 if(currentToken !== "")
-                    tokens.push(currentToken+"\"")
-                currentToken = ""
-                c = index
+                    tokens.push(currentToken+"\"");
+                currentToken = "";
+                c = index;
             }
             else if(command[c] !== " "){
-                currentToken += command[c]
+                currentToken += command[c];
             }else if(command[c] === " " && currentToken !== ""){
-                tokens.push(currentToken)
-                currentToken = ""
+                tokens.push(currentToken);
+                currentToken = "";
             }
         }
         if(currentToken !== ""){
-            tokens.push(currentToken)
+            tokens.push(currentToken);
         }
-        return tokens
-    }
+        return tokens;
+    };
     
     self.hasAlias = (alias) => NodeIT.hasGlobal("alias$$"+alias);
     self.getAlias = (alias) => NodeIT.global("alias$$"+alias);
@@ -92,11 +92,12 @@ var TerminalAction = (()=>{
     self.defineAlias = (value) => {
         var alias = value.replace("alias:","").trim().split("=");
         NodeIT.global("alias$$"+alias[0].trim(),alias[1].trim());
-    }
-    self.onkeyup = (value) => {}
-    self.init = (txtBox) => {txtBox.value = ""}
-    self.getLabelAction = () => "$"
+    };
+    self.onkeyup = (value) => {};
+    
+    self.init = (txtBox) => {txtBox.value = ""};
+    self.getLabelAction = () => "$";
         
-    Shortcut.bindEvent("terminal",{win: "Ctrl+Shift-Z", mac: "Command+Shift-Z"},self)
+    Shortcut.bindEvent("terminal",{win: "Ctrl+Shift-Z", mac: "Command+Shift-Z"},self);
     return self;
 })();
