8aef3aed92e4b4beaf5ca40b3e083188e69ec526
Philippe Moneda <philippemoneda@gmail.com>
2016-03-05 18:18:46 -0300

    ajustes

--- _editor/actions/rightside.js
+++ _editor/actions/rightside.js
@@ -2,9 +2,16 @@ var RightSideAction = (()=>{
     var self = {};
     self.stack = [];
     self.execute = (value) => {
+        var rightSideObj = {};
+        rightSideObj.url = value;
+        rightSideObj.width = 40;
+        self.stack.push(rightSideObj);
+        
         self.setUrl(value);
         ProportionAction.execute("40");
     };
+    
+    
     self.setUrl = (url) => {
        var doc = window.parent.document;
        doc.getElementById("rightSideFrame").setAttribute('src',url);
@@ -33,13 +40,13 @@ var RightSideAction = (()=>{
             ProportionAction.execute("19");
         }
     });
-    Shortcut.bindEvent("hide right side",{mac:"Command+Right", win:"Ctrl+Right"},{
+    Shortcut.bindEvent("hide right side",{mac:"Option+Shift+Right", win:"Alt+Shift+Right"},{
         action:()=>{
            ProportionAction.execute(0);
         }
     });
     
-    Shortcut.bindEvent("show right side",{mac:"Command+Left", win:"Ctrl+Left"},{
+    Shortcut.bindEvent("show right side",{mac:"Option+Shift+Left", win:"Alt+Shift+Left"},{
         action:()=>{
            ProportionAction.execute(30);
         }
