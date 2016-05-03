134b0b4c9ec29fbb03b4d9100bf82d9a05beaac0
Philippe Moneda <philippemoneda@gmail.com>
2016-05-03 20:47:53 -0300

    using ace session to manage opened files

--- _editor/file/fileutils.js
+++ _editor/file/fileutils.js
@@ -33,13 +33,7 @@ var FileUtils = (function(){
             fd.current = true;
             fd.dirty = false;
             fd.session = ace.createEditSession(fd.data,getMode(fd.extension));
-            fd.pos = {};
-            fd.pos.row = 1;
-            fd.pos.column = 0;
-            fd.undoManager = {};
-            fd.undoManager.$undoStack = [];
-            fd.undoManager.$redoStack = [];
-            fd.undoManager.dirtyCounter  = 0;
+            fd.session.on('change',needToSave);
             currentFile = openedFiles.length - 1;
             self.openInEditor(fd);
         });
@@ -56,38 +50,9 @@ var FileUtils = (function(){
     self.saveLocalOpenedFiles = (files) => {
         Splitty.prop("localFilesOpended",JSON.stringify(files));
     };
-    
-    
-    var bindChangeAce = (session) => {
-        if(typeof(session) !== "undefined"){
-           session.on('change',needToSave);
-           session.on('changeSelection',changeCursor);
-       }else{
-          _editor.session.on('change',needToSave);
-          _editor.on('changeSelection',changeCursor);
-       }
-       
-    }
-    
-    var bindChangeSelection = (session) => {
-        _editor.on('changeSelection',changeCursor);
-    }
-    
-    function changeCursor(){
-        var fd = openedFiles[self.currentFileIndex()];
-        if(fd){
-            fd.pos = clone(_editor.getCursorPosition());
-            fd.pos.row = fd.pos.row + 1;
-            fd.pos.column = fd.pos.column + 1;
-        }
-    }
-    
     function needToSave(e){
         var fd = openedFiles[self.currentFileIndex()];
-        protectedChangeAce(()=>{
-            fd.dirty = true;
-        });
-        fd.data = editor.getSession().doc.$lines.join("\n");
+        fd.dirty = true;
         Events.fire(EVENTS.FILE_DIRTY,fd);
     }
     
@@ -101,24 +66,6 @@ var FileUtils = (function(){
         
     }
     
-    var unbindingChangeAce = (session) => {
-       if(typeof(session) !== "undefined"){
-           session.removeListener('change',needToSave);
-           session.removeListener('changeSelection',changeCursor);
-       }else{
-        _editor.session.removeListener('change',needToSave);
-       }
-       _editor.removeListener('changeSelection',changeCursor); 
-    }
-    
-    var protectedChangeAce = (callback,session) => {
-        unbindingChangeAce(session);
-        if(typeof(callback) === "function"){
-            callback();
-        }
-        //bindChangeSelection(session);
-        bindChangeAce(session);
-    };
     
     self.save = () => save();
     
@@ -131,33 +78,19 @@ var FileUtils = (function(){
         return JSON.parse(JSON.stringify(obj));
     }
     self.openInEditor = (fd) => {
-        protectedChangeAce(()=> {
             if(!fd) return;
-            var prev = openedFiles.first((e)=> e.current === true );
-            prev.current = false;
-            //save state change
-            prev.undoManager = {};
-            prev.undoManager.$undoStack = clone(_editor.session.$undoManager.$undoStack);
-            prev.undoManager.$redoStack = clone(_editor.session.$undoManager.$redoStack);
-            prev.undoManager.dirtyCounter = clone(_editor.session.$undoManager.dirtyCounter);
+            for(var i=0; i < openedFiles.length;i++){
+                if(openedFiles[i].current){
+                    openedFiles[i].current = false;
+                    break;
+                }
+            }
             fd.current = true;
             var mode = getMode(fd.extension);
             if(fd.session){
                 _editor.setSession(fd.session);
             }
-            _editor.getSession().setMode(mode);
-            _editor.setValue(fd.data);
-            _editor.session.$undoManager.$undoStack = clone(fd.undoManager.$undoStack);
-            _editor.session.$undoManager.$redoStack = clone(fd.undoManager.$redoStack);
-            _editor.session.$undoManager.dirtyCounter = clone(fd.undoManager.dirtyCounter);
-            if(fd.pos.row > 0)
-                _editor.gotoLine(fd.pos.row,fd.pos.column);
-            else
-                _editor.gotoLine(1);
-             Events.fire(EVENTS.FILE_OPEN,fd);
-        },fd.session);
-       
-        
+            Events.fire(EVENTS.FILE_OPEN,fd);
     };
     
     self.openByIndex = (index) => {  
@@ -217,7 +150,6 @@ var FileUtils = (function(){
     
     Events.when(EVENTS.FILE_OPEN,(fd) => {
         self.pushLocalFile(fd.fileName);
-        bindChangeSelection(fd.session);
     });
     
     Events.when(EVENTS.FILE_CLOSE,(fd) => {
